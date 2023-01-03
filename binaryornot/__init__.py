16ed205229f1173da8618b40a75591b84233682e
pl0mo <76693623+pl0mo@users.noreply.github.com>
2023-01-03 17:53:05 +0000

    - Pre check if file is a directory or not. - Support for many file paths as arguments.

--- binaryornot/__init__.py
+++ binaryornot/__init__.py
@@ -1,3 +1,4 @@
-__author__ = 'Audrey Roy'
+"""BinaryOrNot main module."""
+__authors__ = 'Audrey Roy'
 __email__ = 'audreyr@gmail.com'
-__version__ = '0.4.4'
+__version__ = '0.4.5'
--- binaryornot/check.py
+++ binaryornot/check.py
@@ -6,46 +6,47 @@ binaryornot.check
 
 Main code for checking if a file is binary or text.
 """
-
-import logging
 import argparse
+import logging
+from pathlib import Path
 
 from binaryornot.helpers import get_starting_chunk, is_binary_string
-
 
 logger = logging.getLogger(__name__)
 
 
-def is_binary(filename):
+def is_binary(file_path: Path):
     """
-    :param filename: File to check.
+    :param file_path: File to check.
     :returns: True if it's a binary file, otherwise False.
     """
-    logger.debug('is_binary: %(filename)r', locals())
+    if file_path.is_file():
+        debug_msg = f'is_binary: {file_path}'
+        logger.debug(debug_msg)
 
-    # Check if the file extension is in a list of known binary types
-#     binary_extensions = ['.pyc', ]
-#     for ext in binary_extensions:
-#         if filename.endswith(ext):
-#             return True
+        # Check if the file extension is in a list of known binary types
+        #     binary_extensions = ['.pyc', ]
+        #     for ext in binary_extensions:
+        #         if filename.endswith(ext):
+        #             return True
 
-    # Check if the starting chunk is a binary string
-    chunk = get_starting_chunk(filename)
-    return is_binary_string(chunk)
+        # Check if the starting chunk is a binary string
+        chunk = get_starting_chunk(file_path)
+        return is_binary_string(chunk)
+
+    return False
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Check if a "
-                                                 "file passed as argument is "
-                                                 "binary or not")
-
-    parser.add_argument("filename", help="File name to check for. If "
-                                         "the file is not in the same folder, "
-                                         "include full path")
+    """Starting point."""
+    desc = 'Check if a supplied file path is binary or not.'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('path', nargs='+', help='File path to check for.')
 
     args = parser.parse_args()
 
-    print(is_binary(**vars(args)))
+    for fp_path in map(Path, args.path):
+        print(is_binary(fp_path))
 
 
 if __name__ == "__main__":
